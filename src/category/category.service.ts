e9fd80b02ff015c1c9acb17cce203ecfbab8a7d5
Vale1711 <valeria.jacome7107@utc.edu.ec>
2025-04-28 14:41:49 -0500

    Categoria service

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,26 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException} from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { PrismaService } from 'src/prisma.service';
+import { Categoria } from '@prisma/client';
 
 @Injectable()
 export class CategoryService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(private prisma: PrismaService) {}
+
+  async create(createCategoryDto: CreateCategoryDto): Promise<Categoria> {
+    return this.prisma.categoria.create({
+      data: {
+        nombre: createCategoryDto.nombre,
+      },
+    });
+  }
+   
+  async findAll(): Promise<Categoria[]> {
+    return this.prisma.categoria.findMany();
   }
 
-  findAll() {
-    return `This action returns all category`;
+  async findOne(id: string): Promise<Categoria> {
+    const categoryFound = await this.prisma.categoria.findUnique({
+      where: { id },
+    });
+
+    if (!categoryFound) {
+      throw new NotFoundException('Categoría no encontrada');
+    }
+
+    return categoryFound;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  async update(id: string, updateCategoryDto: UpdateCategoryDto): Promise<Categoria> {
+    const categoryFound = await this.prisma.categoria.findUnique({
+      where: { id },
+    });
+  
+    if (!categoryFound) {
+      throw new NotFoundException(`No se encontró la categoría con la id ${id}`);
+    }
+  
+    const dataToUpdate = Object.fromEntries(
+      Object.entries(updateCategoryDto).filter(([_, value]) => value !== undefined)
+    );
+  
+    const updatedCategory = await this.prisma.categoria.update({
+      where: { id },
+      data: dataToUpdate,
+    });
+  
+    return updatedCategory;
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+  async remove(id: string): Promise<Categoria> {
+    try {
+      const deletedCategory = await this.prisma.categoria.delete({
+        where: { id },
+      });
+      return deletedCategory;
+    } catch (error) {
+      throw new NotFoundException(`Categoría con la id ${id} no encontrada`);
+    }
   }
-
-  remove(id: number) {
-    return `This action removes a #${id} category`;
-  }
+  
 }
