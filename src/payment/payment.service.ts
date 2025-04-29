57a919a24bc946a564d703de28352a34a8bc3b70
Torres1368 <roberth.torres1368@utc.edu.ec>
2025-04-29 18:05:00 -0500

    add payment service

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { UpdatePaymentDto } from './dto/update-payment.dto';
 import { Payment } from './entities/payment.entity';
 import { PrismaService } from 'src/prisma.service';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class PaymentService {
@@ -19,23 +20,49 @@ export class PaymentService {
     return this.prisma.pago.findMany();
   }
 
-  async findOne(id: string):Promise<Payment | null> {
+  async findOne(id: string):Promise<Payment> {
     const paymentFound = await this.prisma.pago.findUnique({
       where: {
         id: id,
       },
     });
     if(!paymentFound){
-      throw new Error('Pago no encontrado');
+      throw new NotFoundException('Pago no encontrado');
     }
     return paymentFound;
   }
 
-  update(id: number, updatePaymentDto: UpdatePaymentDto) {
-    return `This action updates a #${id} payment`;
+  async update(id: string, updatePaymentDto: UpdatePaymentDto):Promise<Payment> {
+    const paymentFound = await this.prisma.pago.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    if(!paymentFound){
+      throw new NotFoundException('Pago no encontrado');
+    }
+
+    const dataToUpdate = Object.fromEntries(
+      Object.entries(updatePaymentDto).filter(([_, value]) => value !== undefined)
+    );
+
+    const updatedPayment = await this.prisma.pago.update({
+      where: { id },
+      data: dataToUpdate,
+    });
+  
+    return updatedPayment;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} payment`;
+
+  async remove(id: string):Promise<Payment> {
+    try{
+      const deletedPayment = await this.prisma.pago.delete({
+        where: {id},
+      });
+      return deletedPayment;
+    }catch(error){
+      throw new NotFoundException(`Pago con la id ${id} no encontrado`);
+    }
   }
 }
