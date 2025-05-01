ecded9c77b258e0141be495d94d9b32aaf542638
Torres1368 <roberth.torres1368@utc.edu.ec>
2025-05-01 17:13:30 -0500

    sintaxis

--- src/shipping-address/dto/create-shipping-address.dto.ts
+++ src/shipping-address/dto/create-shipping-address.dto.ts
@@ -1,1 +1,18 @@
-export class CreateShippingAddressDto {}
+import { IsString, IsUUID } from "class-validator";
+
+export class CreateShippingAddressDto {
+    @IsUUID()
+    readonly usuarioId: string;
+
+    @IsString()
+    readonly direccion: string;
+
+    @IsString()
+    readonly ciudad: string;
+
+    @IsString()
+    readonly codigoPostal: string; 
+
+    @IsString()
+    readonly pais: string;
+}
